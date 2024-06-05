2fbf1f02c6d3f1e3e5ac33be0db9c8336d6433f3
Filip Malm-Bägén <Filip.bagen@gmail.com>
2024-06-05 16:22:41 +0200

    Update AboutMe component with bold text

--- src/components/AboutMe.tsx
+++ src/components/AboutMe.tsx
@@ -10,21 +10,36 @@ export default function AboutMe() {
           <Card>
             <img src="assets/memoji.png" alt="Memoji" className="h-32" />
             <CardContent>
-              Hey there — my name is Filip and I'm a soon-to-be engineer in
-              Computer Science, living in Sweden. I am an eager, creative, and
-              curious tech geek, currently in my fourth year of the M.Sc.
-              program in Media Technology. I've always been fascinated by the
-              possibilities that Artificial Intelligence offers and I've found
-              that my passion for web development and AI perfectly complements
-              my love for technology.
+              <span className="font-semibold">Hey there</span> — my name is
+              Filip and I'm a{' '}
+              <span className="font-semibold">soon-to-be engineer</span> in{' '}
+              <span className="font-semibold">Computer Science</span>, living in
+              Sweden. I am an <span className="font-semibold">eager</span>,{' '}
+              <span className="font-semibold">creative</span>, and{' '}
+              <span className="font-semibold">curious tech geek</span>,
+              currently in my fourth year of the M.Sc. program in Media
+              Technology. I've always been fascinated by the possibilities that{' '}
+              <span className="font-semibold">Artificial Intelligence</span>{' '}
+              offers and I've found that my passion for{' '}
+              <span className="font-semibold">Web Development</span> and{' '}
+              <span className="font-semibold">AI</span>
+              perfectly complements my love for technology.
               <br />
               <br />
-              When I'm not studying, I build my own full-stack applications
-              using my skills in UX, AI, and code development. My aim is to be
-              at the center of new technological developments, working with the
-              great combination of software and science. My focus is to broaden
-              my knowledge in the implementation of new innovations and
-              technological creations.
+              When I'm not studying, I{' '}
+              <span className="font-semibold">
+                build my own full-stack applications
+              </span>
+              using my skills in <span className="font-semibold">UX</span>,{' '}
+              <span className="font-semibold">AI</span>, and{' '}
+              <span className="font-semibold">code development</span>. My aim is
+              to be at the center of new{' '}
+              <span className="font-semibold">technological developments</span>,
+              working with the great combination of{' '}
+              <span className="font-semibold">software</span> and{' '}
+              <span className="font-semibold">science</span>. My focus is to
+              broaden my knowledge in the implementation of new innovations and
+              <span className="font-semibold">technological creations</span>.
             </CardContent>
           </Card>
 
@@ -33,19 +48,29 @@ export default function AboutMe() {
               <CardTitle>I am currently...</CardTitle>
               <CardContent>
                 ... preparing for my final year at university, where I will be
-                studying Artificial Intelligence and Machine Learning at UC
-                Berkeley, California. I am always working on my own projects and
-                looking for an internship where I can apply my skills in
-                practice.
+                studying{' '}
+                <span className="font-semibold">
+                  Artificial Intelligence and Machine Learning
+                </span>{' '}
+                at{' '}
+                <span className="font-semibold">UC Berkeley, California</span>.
+                I am always working on my own projects and looking for an
+                internship where I can apply my skills in practice.
               </CardContent>
             </Card>
 
             <Card>
               <CardTitle>I am looking for...</CardTitle>
               <CardContent>
-                ... an opportunity to write my Master's thesis at a company
-                where I can both learn and gain as much experience as possible
-                and also get the chance to implement my own ideas.
+                ... an opportunity to write my{' '}
+                <span className="font-semibold">Master's thesis</span> at a
+                company where I can both{' '}
+                <span className="font-semibold">learn</span> and{' '}
+                <span className="font-semibold">
+                  gain as much experience as possible
+                </span>{' '}
+                and also get the chance to{' '}
+                <span className="font-semibold">implement my own ideas</span>.
               </CardContent>
             </Card>
           </div>
