60516213249d54cbd81b06b93ac4cf046e7d79b8
Filip <filip.bagen@netlight.com>
2026-04-23 11:40:56 +0200

    Refactor Badge component by removing unnecessary border styles for a cleaner design.

--- src/components/ui/Badge.tsx
+++ src/components/ui/Badge.tsx
@@ -7,7 +7,7 @@ interface BadgeProps {
 
 const Badge: React.FC<BadgeProps> = ({ name }) => {
   return (
-    <div className="w-fit px-2.5 py-0.5 rounded-full font-medium bg-[#251434]/10 dark:bg-[#E0BDFF]/10 border-2 border-[#251434]/20 dark:border-[#E0BDFF]/20">
+    <div className="w-fit px-2.5 py-0.5 rounded-full font-medium bg-[#251434]/10 dark:bg-[#E0BDFF]/10">
       <p className="text-xs">{name}</p>
     </div>
   );
